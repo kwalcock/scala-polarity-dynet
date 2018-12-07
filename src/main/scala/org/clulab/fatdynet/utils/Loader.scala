40adfc7e38e6562d04009bd0caf5e6e7fa8f9584
Keith Alcock <github@keithalcock.com>
2018-12-06 22:06:56 -0700

    Try to load model

--- src/main/scala/org/clulab/fatdynet/utils/Loader.scala
+++ src/main/scala/org/clulab/fatdynet/utils/Loader.scala
@@ -8,32 +8,47 @@ import scala.io.Source
 
 object Loader {
 
-  class ClosableModelLoader(filename: String) extends ModelLoader(filename) {
+  protected class ClosableModelLoader(filename: String) extends ModelLoader(filename) {
     def close(): Unit = done
   }
 
-  // Add version with it's own, open model loader than can be used later
-  def loadExpressions(path: String, nameSpace: String = "/"): Map[String, Expression] = {
+  // This converts an objectType and objectName into a decision about whether
+  // to further process the line.  It can use the ModelLoader to do some
+  // processing itself.  See falseModelFilter and loadLstm for examples.
+  protected type ModelFilter = (ModelLoader, String, String) => Boolean
 
-    def read(line: String, modelLoader: ModelLoader, pc: ParameterCollection): (String, Expression) = {
+  protected def falseModelFilter(modelLoader: ModelLoader, objectType: String, objectName: String) = {
+    // Skip these kinds of thing because they are likely a model of some kind.
+    !(objectType == "#Parameter#" && objectName.matches(".*/_[0-9]+$"))
+  }
+
+  def loadExpressions(path: String, namespace: String = ""): Map[String, Expression] =
+      filteredLoadExpressions(path, namespace, falseModelFilter)
+
+  protected def filteredLoadExpressions(path: String, namespace: String = "", modelFilter: ModelFilter = falseModelFilter): Map[String, Expression] = {
+
+    def read(line: String, modelLoader: ModelLoader, pc: ParameterCollection): Option[(String, Expression)] = {
       // See https://dynet.readthedocs.io/en/latest/python_saving_tutorial.html
       val Array(objectType, objectName, dimension, _, _) = line.split(" ")
-      // Skip leading { and trailing }
-      val dims = dimension.substring(1, dimension.length - 1).split(",").map(_.toInt)
-      val expression = objectType match {
-        case "#Parameter#" =>
-          val param = pc.addParameters(Dim(dims))
-          param.dim().get(0)
-          modelLoader.populateParameter(param, key = objectName)
-          Expression.parameter(param)
-        case "#LookupParameter#" => // if name begins with modelName?
-          val param = pc.addLookupParameters(dims(1), Dim(dims(0)))
-          modelLoader.populateLookupParameter(param, key = objectName)
-          Expression.parameter(param)
-        case _ => throw new RuntimeException("Unrecognized line in model file")
-      }
 
-      (objectName, expression)
+      if (objectName.startsWith(namespace) && modelFilter(modelLoader, objectType, objectName)) {
+        // Skip leading { and trailing }
+        val dims = dimension.substring(1, dimension.length - 1).split(",").map(_.toInt)
+        val expression = objectType match {
+          case "#Parameter#" =>
+            val param = pc.addParameters(Dim(dims))
+            modelLoader.populateParameter(param, key = objectName)
+            Expression.parameter(param)
+          case "#LookupParameter#" =>
+            val param = pc.addLookupParameters(dims.last, Dim(dims.dropRight(1)))
+            modelLoader.populateLookupParameter(param, key = objectName)
+            Expression.parameter(param)
+          case _ => throw new RuntimeException(s"Unrecognized line in model file: '$line'")
+        }
+        Option((objectName, expression))
+      }
+      else
+        None
     }
 
     val expressions = (new ClosableModelLoader(path)).autoClose { modelLoader =>
@@ -43,8 +58,7 @@ object Loader {
         source
             .getLines
             .filter(_.startsWith("#"))
-            .filter(_.contains(nameSpace)) // should check name for this, not entire line
-            .map { line => read(line, modelLoader, pc) }
+            .flatMap { line => read(line, modelLoader, pc) }
             .toMap
       }
     }
@@ -52,24 +66,42 @@ object Loader {
     expressions
   }
 
-//  def loadLstm(expressions: Expression, ) = ???
-  // from a list of expressions?
-  // returns lstmBuilder and expressions?
+  def loadLstm(path: String, namespace: String = ""): (Option[LstmBuilder], Map[String, Expression]) = {
+    var lstmBuilder: Option[LstmBuilder] = None
 
+    def lstmModelFilter(modelLoader: ModelLoader, objectType: String, objectName: String) = {
+      if (objectType == "#Parameter#") {
+        val matches = objectName.matches("(.*)/vanilla-lstm-builder/_[0-9]+$")
+
+        if (matches) {
+          val key = "/keith/vanilla-lstm-builder/"
+          val pc = new ParameterCollection
+          val layers = 1
+          val inputDim = 100
+          val hiddenDim = 20
+          // Do I need to build a parameter collection with each call?
+
+          // Do need to capture the name in regex
+          modelLoader.populateModel(pc, key)
+          lstmBuilder = Option(new LstmBuilder(1, 2, 3, pc))
+          false
+        }
+        else
+          true
+      }
+      else
+        true
+    }
+
+    val expressions = filteredLoadExpressions(path, namespace, lstmModelFilter)
+
+    (lstmBuilder, expressions)
+  }
 
   def main(args: Array[String]): Unit = {
     Initialize.initialize(Map("random-seed" -> 2522620396l))
 
-    val expressions: Map[String, Expression] = loadExpressions("model.dy") // , "/vanilla-lstm-builder/")
-
-    val numLayers = expressions("/V").dim.get(0) // find lstm and _0, _1, _2, how many are there, and subtract input and output
-    val wemDimensions = expressions("/wemb").dim.get(0)
-    val hiddenDim = expressions("/W").dim.get(1)
-    val pc = new ParameterCollection()
-
-
-    // It should return lstm builder
-    val builder = new LstmBuilder(numLayers, wemDimensions, hiddenDim, pc)
+    val (builder, expressions) = loadLstm("model.dy.kwa") // , "/vanilla-lstm-builder/")
 
     expressions.keys.foreach(println)
   }
