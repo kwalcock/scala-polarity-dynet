2b1c62e4c109a5b027c0c03eb2a822332354211b
Keith Alcock <github@keithalcock.com>
2018-12-07 12:53:52 -0700

    Add transducer

--- src/main/scala/org/clulab/fatdynet/utils/Loader.scala
+++ src/main/scala/org/clulab/fatdynet/utils/Loader.scala
@@ -1,11 +1,43 @@
 package org.clulab.fatdynet.utils
 
-import edu.cmu.dynet.{Dim, Expression, Initialize, LookupParameter, LstmBuilder, ModelLoader, ModelSaver, Parameter, ParameterCollection}
+import edu.cmu.dynet.{
+  Initialize,
+
+  Dim,
+  Expression,
+
+  LookupParameter,
+  Parameter,
+  ParameterCollection,
+
+  // These other, unused builders to be addedd
+
+  FastLstmBuilder,
+  LstmBuilder,
+    CompactVanillaLSTMBuilder,
+    CoupledLstmBuilder,
+    VanillaLstmBuilder,
+
+  TreeLSTMBuilder, // abstract
+    UnidirectionalTreeLSTMBuilder,
+    BidirectionalTreeLSTMBuilder,
+
+  RnnBuilder, // abstract
+    SimpleRnnBuilder,
+
+  GruBuilder,
+
+  ModelLoader,
+  ModelSaver,
+}
 
 import org.clulab.fatdynet.utils.Closer.AutoCloser
 
 import scala.io.Source
 
+/**
+  * See https://dynet.readthedocs.io/en/latest/python_saving_tutorial.html
+  */
 object Loader {
 
   protected class ClosableModelLoader(filename: String) extends ModelLoader(filename) {
@@ -16,12 +48,13 @@ object Loader {
     def close(): Unit = done
   }
 
-  // This converts an objectType and objectName into a decision about whether
-  // to further process the line.  It can use the ModelLoader to do some
-  // processing itself.  See falseModelFilter and loadLstm for examples.
+  /** This converts an objectType and objectName into a decision about whether
+    * to further process the line.  It can use the ModelLoader to do some
+    * processing itself.  See falseModelFilter and loadLstm for examples.
+    */
   protected type ModelFilter = (ModelLoader, String, String, Array[Int]) => Boolean
 
-  protected def falseModelFilter(modelLoader: ModelLoader, objectType: String, objectName: String, dims: Array[Int]) = {
+  protected def falseModelFilter(modelLoader: ModelLoader, objectType: String, objectName: String, dims: Array[Int]): Boolean = {
     // Skip these kinds of thing because they are likely a model of some kind.
     !(objectType == "#Parameter#" && objectName.matches(".*/_[0-9]+$"))
   }
@@ -32,12 +65,11 @@ object Loader {
   protected def filteredLoadExpressions(path: String, namespace: String = "", modelFilter: ModelFilter = falseModelFilter): Map[String, Expression] = {
 
     def read(line: String, modelLoader: ModelLoader, pc: ParameterCollection): Option[(String, Expression)] = {
-      // See https://dynet.readthedocs.io/en/latest/python_saving_tutorial.html
       val Array(objectType, objectName, dimension, _, _) = line.split(" ")
+      // Skip leading { and trailing }
       val dims = dimension.substring(1, dimension.length - 1).split(",").map(_.toInt)
 
       if (objectName.startsWith(namespace) && modelFilter(modelLoader, objectType, objectName, dims)) {
-        // Skip leading { and trailing }
         val expression = objectType match {
           case "#Parameter#" =>
             val param = pc.addParameters(Dim(dims))
@@ -55,7 +87,7 @@ object Loader {
         None
     }
 
-    val expressions = (new ClosableModelLoader(path)).autoClose { modelLoader =>
+    val expressions = new ClosableModelLoader(path).autoClose { modelLoader =>
       Source.fromFile(path).autoClose { source =>
         val pc = new ParameterCollection
 
@@ -70,7 +102,11 @@ object Loader {
     expressions
   }
 
-  def loadLstm(path: String, namespace: String = ""): (Option[LstmBuilder], Map[String, Expression]) = {
+  // More of these will be added, plus design will change if the format can be generalized.
+  def loadLstm(path: String, namespace: String = ""): (Option[LstmBuilder], Map[String, Expression]) = ???
+  def loadCoupledLstm(path: String, namespace: String = ""): (Option[CoupledLstmBuilder], Map[String, Expression]) = ???
+
+  def loadVanillaLstm(path: String, namespace: String = ""): (Option[VanillaLstmBuilder], Map[String, Expression]) = {
     val model = new ParameterCollection
     var inputDim = -1
     var hiddenDim = -1
@@ -93,28 +129,33 @@ object Loader {
         true
     }
 
-    // Could throw exception rather than use option
+    // Could throw exception rather than use option or turn this into a collection.
     val expressions = filteredLoadExpressions(path, namespace, lstmModelFilter)
     val lstmBuilder =
         if (layers >= 2)
-          Option(new LstmBuilder(layers - 2, inputDim, hiddenDim, model))
+          Option(new VanillaLstmBuilder(layers - 2, inputDim, hiddenDim, model))
         else
           None
 
     (lstmBuilder, expressions)
   }
 
+  val W2V_SIZE = 1234
+  val VOC_SIZE = 3671
+
+  val WEM_DIMENSIONS = 100
+  val NUM_LAYERS = 1
+  val FF_HIDDEN_DIM = 10
+  val HIDDEN_DIM = 20
+
+  val W_KEY = "/W"
+  val B_KEY = "/b"
+  val V_KEY = "/V"
+  val W2V_WEMB_KEY = "/w2v-wemb"
+  val MISSING_WEB_KEY = "/missing-wemb"
+
   def write(filename: String): Unit = {
-    val VOC_SIZE = 3671
-    val W2V_SIZE = 1234
-
-    val WEM_DIMENSIONS = 100
-    val NUM_LAYERS = 1
-    val FF_HIDDEN_DIM = 10
-    val HIDDEN_DIM = 20
-
     val pc = new ParameterCollection
-    val model = new ParameterCollection
 
     val W_p: Parameter = pc.addParameters(Dim(Seq(FF_HIDDEN_DIM, HIDDEN_DIM)))
     val b_p: Parameter = pc.addParameters(Dim(Seq(FF_HIDDEN_DIM)))
@@ -123,45 +164,56 @@ object Loader {
     val w2v_wemb_lp: LookupParameter = pc.addLookupParameters(W2V_SIZE, Dim(Seq(WEM_DIMENSIONS)))
     val missing_wemb_lp: LookupParameter = pc.addLookupParameters(VOC_SIZE, Dim(Seq(WEM_DIMENSIONS)))
 
-    val builder = new LstmBuilder(NUM_LAYERS, WEM_DIMENSIONS, HIDDEN_DIM, model)
+    val model = new ParameterCollection
+    /*val builder = */ new VanillaLstmBuilder(NUM_LAYERS, WEM_DIMENSIONS, HIDDEN_DIM, model)
 
-    (new ClosableModelSaver(filename)).autoClose { saver =>
-      saver.addParameter(W_p, "/W")
-      saver.addParameter(b_p, "/b")
-      saver.addParameter(V_p, "/V")
-      saver.addLookupParameter(w2v_wemb_lp, "/w2v-wemb")
-      saver.addLookupParameter(missing_wemb_lp, "/missing-wemb")
+    new ClosableModelSaver(filename).autoClose { saver =>
+      saver.addParameter(W_p, W_KEY)
+      saver.addParameter(b_p, B_KEY)
+      saver.addParameter(V_p, V_KEY)
+      saver.addLookupParameter(w2v_wemb_lp, W2V_WEMB_KEY)
+      saver.addLookupParameter(missing_wemb_lp, MISSING_WEB_KEY)
       saver.addModel(model)
     }
   }
 
+  // May have to have several versions to accounts for all builders
+  def transduce(builder: RnnBuilder, inputs: Iterable[Expression]): Option[Expression] =
+    inputs.foldLeft(None: Option[Expression]){ (_, input) => Some(builder.addInput(input)) }
+  //        if (inputs.size == 0)
+  //          None
+  //        else {
+  //          inputs.dropRight(1).foreach { builder.addInput(_) }
+  //          Some(builder.addInput(inputs.last))
+  //          }
+
   def read(filename: String): Unit = {
-    val (optionBuilder, expressions) = loadLstm(filename)
+    val (optionBuilder, expressions) = loadVanillaLstm(filename)
 
     expressions.keys.foreach(println)
 
     val builder = optionBuilder.get
-    val V = expressions("/V")
-    val W = expressions("/W")
-    val b = expressions("/b")
+    val W = expressions(W_KEY)
+    val b = expressions(B_KEY)
+    val V = expressions(V_KEY)
+    val w2v_wemb = expressions(W2V_WEMB_KEY)
+    val missing_wemb = expressions(MISSING_WEB_KEY)
 
-    val pc = new ParameterCollection
-    // Can this be different?
-    val WEM_DIMENSIONS = 100
-    val w2v_wemb_lp: LookupParameter = pc.addLookupParameters(1000 /*584550 1579375*/, Dim(Seq(WEM_DIMENSIONS)))
-
-    val inputs = 0.until(100) map { Expression.lookup(w2v_wemb_lp, _)}
+    // An example run...
+    val inputs = 0.until(100) map { index =>
+      if (index % 2 == 0)
+        Expression.pick(w2v_wemb, index % W2V_SIZE, 1) // size implied from eventual dictionary
+      else
+        Expression.pick(missing_wemb, index % VOC_SIZE, 1) // size implied from eventual dictionary
+    }
 
     builder.newGraph()
     builder.startNewSequence()
 
-    val states = inputs map {
-      w => builder.addInput(w)
-    }
-    val selected = states.last
+    val selected = transduce(builder, inputs).get
     val prediction = Expression.logistic(V * (W * selected + b))
 
-    println(prediction)
+    print(prediction.value().toSeq())
   }
 
   def main(args: Array[String]): Unit = {
